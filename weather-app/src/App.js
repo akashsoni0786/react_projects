2530011e6d067c907093b98bc72ea48200dfdc6a
akashsoni0786 <akashsoni@cedcoss.com>
2022-08-05 12:45:01 +0530

    Done

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
-import React from "react";
-import "./Final.css"
+import React, { useEffect } from "react";
+import "./Final.css";
+
 function App() {
   const conditions = [
     "Sunny",
@@ -11,15 +12,27 @@ function App() {
     "Partly cloudy",
   ];
   const [weather, setWeather] = React.useState([]);
-  const [search, setSearch] = React.useState("");
-  const [city, setCity] = React.useState("");
-  const [temp, setTemp] = React.useState("");
-  const [wind, setWind] = React.useState("");
-  const [date, setDate] = React.useState("");
-  const [icon, setIcon] = React.useState("");
+  const [search, setSearch] = React.useState("lucknow");
+  const [theme, setTheme] = React.useState("sunny");
 
-  const [humidity, setHumidity] = React.useState("");
-  React.useEffect(() => {
+  const fetchApi = async () => {
+    const url = `https://api.weatherapi.com/v1/current.json?key=0bab7dd1bacc418689b143833220304&q=${search}`;
+    const response = await fetch(url);
+    const resjson = response.json();
+    resjson
+      .then((result) => {
+        debugger;
+        if (result.error) {
+          throw new Error(result.error.message);
+        }
+        setWeather(result);
+      })
+      .catch((e) => {
+        alert(e.toString());
+      });
+  };
+
+  useEffect(() => {
     const fetchApi = async () => {
       try {
         const url = `https://api.weatherapi.com/v1/current.json?key=0bab7dd1bacc418689b143833220304&q=${search}`;
@@ -27,80 +40,100 @@ function App() {
         const resjson = response.json();
         resjson.then((result) => {
           setWeather(result);
+          backtheme();
+          console.log(result);
         });
       } catch (e) {
         alert("Not found");
       }
     };
     fetchApi();
- 
-  },[search]);
+  }, []);
 
-  const show = () => {
-  
+  const show = (event) => {
+    event.preventDefault();
     if (search == "") {
-      alert("Field is empty");
+      console.log("Field is empty");
     } else {
       console.log(weather);
-      let humiditys = weather.current.humidity;
-      let temp = Number(weather.current.temp_c);
-      let wind = weather.current.wind_kph;
-      let cityname = weather.location.name;
-      setDate(weather.current.condition.text);
-      setIcon(weather.current.condition.icon);
-      setHumidity(humiditys);
-      setCity(cityname);
-      setTemp(temp);
-      setWind(wind);
+
+      fetchApi();
+      backtheme();
     }
   };
- 
+
+  const backtheme = () => {
+    if (weather.current.condition.text.includes("rain")) {
+      setTheme("rain")
+    }
+    if (weather.current.condition.text.includes("sunny")) {
+      setTheme("sunny")
+    }
+    if (weather.current.condition.text.includes("overcast")) {
+      setTheme("overcast")
+    }
+    if (weather.current.condition.text.includes("cloud")) {
+      setTheme("cloud")
+    }
+    if (weather.current.condition.text.includes("mist")) {
+      setTheme("mist")
+    }
+    if (weather.current.condition.text.includes("fog")) {
+      setTheme("fog")
+    }
+  };
 
   return (
-
-    <div>
+    <div className="body" style={{ backgroundImage: "url(https://source.unsplash.com/1600x900/?" + theme + ")" }}>
       <div className="card">
-        <div className="search">
-          <input type="text"
-          onChange={(e) => {
-            setSearch(e.target.value);
-          }}
-          className="searchbar"
-           placeholder="Search" />
-          <button onClick={show}>
-            <svg
-            onClick={show}
-              stroke="currentColor"
-              fill="currentColor"
-              stroke-width="0"
-              viewBox="0 0 1024 1024"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path  d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0 0 11.6 0l43.6-43.5a8.2 8.2 0 0 0 0-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z"></path>
-            </svg>
-          </button>
-        </div>
-          {/* {weather.location == [] ? <p>Loding......</p>:( */}
-            <div className="weather">
-          <h2 className="city">Weather in {city}</h2>
-          <h1 className="temp">{temp}&#8451;</h1>
-          <div className="flex">
-            <img
-              alt=""
-              src={icon}
-              className="icon"
-            ></img>
-            <div className="description">{date}</div>
+        <form onSubmit={show}>
+          <div className="search">
+            <input
+              type="text"
+              onChange={(e) => {
+                setSearch(e.target.value);
+              }}
+              className="searchbar"
+              placeholder="Search"
+            />
+            <button id="showbtn">
+              <svg
+                stroke="currentColor"
+                fill="currentColor"
+                stroke-width="0"
+                viewBox="0 0 1024 1024"
+                height="1em"
+                width="1em"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0 0 11.6 0l43.6-43.5a8.2 8.2 0 0 0 0-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z"></path>
+              </svg>
+            </button>
           </div>
-          <div className="humidity">Humidity: {humidity}%</div>
-          <div className="wind">Wind speed: {wind} km/h</div>
-        </div>
-          {/* )} */}
-        
+        </form>
 
-
+        {Object.keys(weather).length > 0 && (
+          <div className="weather">
+            <h2 className="city">Weather in {weather.location.name}</h2>
+            <h1 className="temp">{weather.current.temp_c}&#8451;</h1>
+            <div className="flex">
+              <img
+                alt=""
+                src={weather.current.condition.icon}
+                className="icon"
+              ></img>
+              <div className="description">
+                {weather.current.condition.text}
+              </div>
+            </div>
+            <div className="humidity">
+              Humidity: {weather.current.humidity}%
+            </div>
+            <div className="wind">
+              Wind speed: {weather.current.wind_kph} km/h
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
